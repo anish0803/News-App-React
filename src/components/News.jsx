1cf097280721cc885b4d181ba1b2186e7b020cd1
anish0803 <105308559+anish0803@users.noreply.github.com>
2022-10-20 21:03:00 +0530

    import self api

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -2,23 +2,30 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import './News.css'
 
-export default function News() {
+export default function News(props) {
 
     const [data,setData] = useState([]);
 
     useEffect(()=>{
         axios
-            .get(`https://newsapi.org/v2/everything?q=keyword&apiKey=3064d88d2d1345a7915d57781a10a49e`)
+            .get(`https://newsapi.org/v2/everything?q=tesla&from=2022-10-20&sortBy=publishedAt&apiKey=e5851bb345f24089aecb3088fe55c057`)
             .then((response)=>{
                 let result = response.data.articles;
                 setData([data,...result]);
-                // console.log(result)
+                console.log(result)
             })
     },[])
 
 
   return (
    <>
+                    <div className="searchInput">
+          <input type="text" class="css-input" placeholder="enter news here" onChange={(e) => {
+              props.getfun(e.target.value);
+            }} />
+          <button className="inputSubmit">Submit</button>
+        </div>
+
         {data.map((item,index)=>{
                 return(
                     <div className='articles' key={index} >
